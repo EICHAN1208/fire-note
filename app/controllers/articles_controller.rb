a6a0e50988e92fde14125e00ad334b1ebbf7e2a8
村上　瑛基 <ek.h1.hy@gmail.com>
2020-05-03 12:43:37 +0900

    記事の編集・削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +13,6 @@ class ArticlesController < ApplicationController
 
   def create
     @article = current_user.articles.new(article_params)
-
     if @article.save
       redirect_to articles_path, notice: "記事を投稿しました！"
     else
@@ -21,6 +21,25 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: "記事を更新しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy!
+      redirect_to root_path, notice: "記事を削除しました！"
+    else
+      render :show
+    end
   end
 
   private
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,10 +3,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'unauthorized'
 
-  protected
-  def after_update_path_for(resource)
-    user_path
-  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
