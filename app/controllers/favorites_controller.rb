0e6ac89666d81e3bc55fb3c995ec2daa48b5b272
村上　瑛基 <ek.h1.hy@gmail.com>
2020-05-10 00:06:48 +0900

    いいねのAjax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
   def create
+    @article = Article.find(params[:article_id]) #create.js.erbで必要
     favorite = current_user.favorites.build(article_id: params[:article_id])
     favorite.save
-    redirect_to article_path(favorite.article_id)
+    # redirect_to article_path(favorite.article_id)
   end
 
   def destroy
+    @article = Article.find(params[:article_id]) #create.js.erbで必要
     favorite = Favorite.find_by(article_id: params[:article_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to article_path(favorite.article_id)
+    # redirect_to article_path(favorite.article_id)
   end
 end
