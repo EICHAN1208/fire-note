5ff7a9830493b0579fbf276b057d204316131c47
村上　瑛基 <ek.h1.hy@gmail.com>
2020-05-10 12:22:59 +0900

    ストックのAjax化

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,13 +1,15 @@
 class StocksController < ApplicationController
   def create
+    @article = Article.find(params[:article_id]) #create.js.erbで必要
     stock = current_user.stocks.build(article_id: params[:article_id])
     stock.save
-    redirect_to article_path(stock.article_id)
+    # redirect_to article_path(stock.article_id)
   end
 
   def destroy
+    @article = Article.find(params[:article_id]) #destroy.js.erbで必要
     stock = Stock.find_by(article_id: params[:article_id], user_id: current_user.id)
     stock.destroy
-    redirect_to article_path(stock.article_id)
+    # redirect_to article_path(stock.article_id)
   end
 end
