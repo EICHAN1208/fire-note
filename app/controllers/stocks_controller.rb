feb964970986d2ba75a39a6a3ba2fc07cc85e92d
村上　瑛基 <ek.h1.hy@gmail.com>
2020-06-13 10:41:00 +0900

    コントローラをリファクタ

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,7 +8,7 @@ class StocksController < ApplicationController
 
   def destroy
     @article = Article.find(params[:article_id]) #destroy.js.erbで必要
-    stock = Stock.find_by(article_id: params[:article_id], user_id: current_user.id)
+    stock = current_user.stocks.find_by(article_id: params[:article_id])
     stock.destroy
     # redirect_to article_path(stock.article_id)
   end
